9538dfc6942c9fd168e45f1ff14f141a5debd9a8
menguel <menguelo.js@gmail.com>
2025-02-25 12:56:08 +0100

    Ajout de json-server dans le projet

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from "redux";
 import userReducer from "./user.reducer";
 import postReducer from "./post.reducer";
 
+// Ici c'est le fichier où l'on combine tout nos reducers (rootReducer)
 
 export default combineReducers({
     // REDUCERS 
